d47467fc3efb022253ea3ad3e5d818be4bba33eb
puminblood <42122302+puminblood@users.noreply.github.com>
2024-11-18 10:40:05 +0100

    Modification des regex pour matcher logs.har

--- manipdock/src/extraction.rs
+++ manipdock/src/extraction.rs
@@ -16,7 +16,7 @@ pub fn extract_id(data: &String) -> io::Result<String>{
 /*
 Extrait l'ID de la requête sous format String
 */
-    let re = Regex::new(r#""id":"([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})""#).unwrap();
+    let re = Regex::new(r#""id\\\":\\\"([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})\\\""#).unwrap();
     if let Some(captures) = re.captures(&data) {
         Ok(captures.get(1).map(|m| m.as_str().to_string()).unwrap())
     } else {
@@ -24,50 +24,51 @@ Extrait l'ID de la requête sous format String
     }
 }
 
-pub fn extract_date(data: &String) -> io::Result<String>{
+pub fn extract_date(data: &String) -> io::Result<String> {
 /*
 Extrait la date de la requête sous format String
 */
-    let re = Regex::new(r"([0-9]{2} [A-Z]{1}[a-z]{2} [0-9]{4})").unwrap();
-    if let Some(captures) = re.captures(&data) {
-        Ok(captures.get(1).map(|m| m.as_str().to_string()).unwrap())
+    let re = Regex::new(r#""startedDateTime\": \"([^"]+)\""#).unwrap();
+    if let Some(captures) = re.captures(data) {
+        Ok(captures.get(1).unwrap().as_str().to_string())
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "Date not found"))
     }
 }
 
-pub fn extract_content(data: &String) -> io::Result<String>{
+pub fn extract_content(data: &String) -> io::Result<String> {
 /*
-Extrait le contenu de la requête sous format String
+Extrait le contenu dans "parts" sous forme de tableau String
 */
-    let re = Regex::new(r#""parts":\["(.*?)"\]"#).unwrap();
-    if let Some(captures) = re.captures(&data) {
-        Ok(captures.get(1).map(|m| m.as_str().to_string()).unwrap())
+    let re = Regex::new(r#""parts\\":\[\\"(.*?)\\"\]"#).unwrap();
+    if let Some(captures) = re.captures(data) {
+        Ok(captures.get(1).unwrap().as_str().to_string())
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "Content not found"))
     }
 }
 
-pub fn extract_user_agent(data: &String) -> io::Result<String>{
+pub fn extract_user_agent(data: &String) -> io::Result<String> {
 /*
-Extrait le user_agent de la requête sous format String
+Extrait le user-agent de la requête sous format String
 */
-    let re = Regex::new(r"User-Agent: (.*)").unwrap();
-    if let Some(captures) = re.captures(&data) {
-        Ok(captures.get(1).map(|m| m.as_str().to_string()).unwrap())
+    let re = Regex::new(r#""user-agent",\s+"value":\s+"([^"]+)""#).unwrap();
+    if let Some(captures) = re.captures(data) {
+        Ok(captures.get(1).unwrap().as_str().to_string())
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "User agent not found"))
     }
 }
 
-pub fn extract_author(data: &String) -> io::Result<String>{
+pub fn extract_author(data: &String) -> io::Result<String> {
 /*
 Extrait l'auteur de la requête sous format String
 */
-    let re = Regex::new(r#""role":"([^"]*)""#).unwrap();
-    if let Some(captures) = re.captures(&data) {
-        Ok(captures.get(1).map(|m| m.as_str().to_string()).unwrap())
+    let re = Regex::new(r#""role\\\":\\\"([^"]+)\\\""#).unwrap();
+    if let Some(captures) = re.captures(data) {
+        Ok(captures.get(1).unwrap().as_str().to_string())
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "Author not found"))
     }
 }
+
