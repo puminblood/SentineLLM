7d90322ca621245afa96ad8490c6f4123f5d30b5
puminblood <42122302+puminblood@users.noreply.github.com>
2024-11-29 14:49:31 +0100

    Stockage des embeddings sur mongoDB

--- embeddings/src/extract.rs
+++ embeddings/src/extract.rs
@@ -1,25 +1,25 @@
 use pdf_extract::extract_text;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Embedding {
     pub text: String,
     pub embedding: Vec<f32>,
-    pub label: i32, // 1 pour "sensible", 0 pour "non sensible"
+    pub label: String, // 1 pour "sensible", 0 pour "non sensible"
 }
 
-pub fn extract_text_and_labels(pdf_path: &str,) -> Result<Vec<(String, i32)>, Box<dyn std::error::Error>> {
+pub fn extract_text_and_labels(pdf_path: &str,) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
     let content = extract_text(pdf_path)?;
     let mut results = Vec::new();
-    let mut label = 0;
+    let mut label = "0".to_string();
 
     for line in content.lines() {
         if line.contains("[SENSIBLE]") {
-            label = 1;
+           label = "1".to_string();
         } else if line.contains("[END]") {
-            label = 0;
+            label = "0".to_string();
         } else {
-            results.push((line.to_string(), label));
+            results.push((line.to_string(), label.clone()));
         }
     }
 
