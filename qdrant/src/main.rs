db476d29308ded20ecb67b20d880d72c1b11b556
puminblood <42122302+puminblood@users.noreply.github.com>
2024-12-05 22:43:46 +0100

    Qdrant test code

--- qdrant/src/main.rs
+++ qdrant/src/main.rs
@@ -1,36 +1,41 @@
-use qdrant_client::qdrant::{PointStruct, UpsertPointsBuilder};
 use qdrant_client::Qdrant;
-use qdrant_client::qdrant::{CreateCollectionBuilder, Distance, VectorParamsBuilder};
+use qdrant_client::qdrant::{
+    Condition, CreateCollectionBuilder, Distance, Filter, PointStruct, ScalarQuantizationBuilder,
+    SearchParamsBuilder, SearchPointsBuilder, UpsertPointsBuilder, VectorParamsBuilder,
+};
 
 
-fn create_collection() -> Result<(), Box<dyn std::error::Error>>{
+async fn _create_collection() -> Result<(), Box<dyn std::error::Error>>{
+    println!("Start création");
     let client = Qdrant::from_url("http://localhost:6334")
     .api_key(std::env::var("QDRANT_API_KEY"))
     .build();
-    client?
+    let response = client?
         .create_collection(
             CreateCollectionBuilder::new("my_collection")
                 .vectors_config(VectorParamsBuilder::new(512, Distance::Cosine)),
-        );
+        ).await?;
+    dbg!(response);
     println!("Création fini");
     Ok(())
 }
 
-async fn _insert_collection() -> Result<(), Box<dyn std::error::Error>>{
+async fn insert_collection() -> Result<(), Box<dyn std::error::Error>>{
+    println!("Start insert");
     let client = Qdrant::from_url("http://localhost:6334")
     .api_key(std::env::var("QDRANT_API_KEY"))
     .build();
 
     let points = vec![
         PointStruct::new(
-            42,                 // Uniqe piont ID
-            vec![0.0_f32; 512], // Vector to upsert
+            43,                 // Uniqe piont ID
+            vec![1.0_f32; 512], // Vector to upsert
             // Attached payload
             [
                 ("great", true.into()),
                 ("level", 9000.into()),
-                ("text", "Hi Qdrant!".into()),
-                ("list", vec![1.234f32, 0.815].into()),
+                ("text", "Hello Qdrant!".into()),
+                ("list", vec![0.9f32, 1.409].into()),
             ],
         ),
     ];
@@ -39,10 +44,31 @@ async fn _insert_collection() -> Result<(), Box<dyn std::error::Error>>{
         .upsert_points(UpsertPointsBuilder::new("my_collection", points))
         .await?;
     dbg!(response);
+    println!("End insert");
+    Ok(())
+}
+
+async fn search_collection() -> Result<(), Box<dyn std::error::Error>>{
+    println!("Start seraching");
+    let client = Qdrant::from_url("http://localhost:6334")
+    .api_key(std::env::var("QDRANT_API_KEY"))
+    .build();
+    let search_result = client?
+    .search_points(
+        SearchPointsBuilder::new("my_collection", [11.; 512], 10)
+            .filter(Filter::all([Condition::matches("text", "Hello Qdrant!".to_string())]))
+            .with_payload(true)
+            .params(SearchParamsBuilder::default().exact(true)),
+    )
+    .await?;
+    dbg!(&search_result);
+    println!("End searching");
     Ok(())
 }
 
 #[tokio::main]
 async fn main(){
-    let _ = create_collection();
+    if let Err(e) = search_collection().await{
+        println!("Result :{}", e);
+    }
 }
